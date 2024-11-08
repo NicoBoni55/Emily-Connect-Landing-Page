56b1a968c418fcb2a0f178d3db0c7c3b29482cb1
NicoBoni55 <bonifrank55@gmail.com>
2024-11-08 13:36:50 -0300

    new page

--- script.js
+++ script.js
@@ -2,11 +2,21 @@ document.addEventListener('scroll', function() {
     const scrollPosition = window.scrollY;
     const body = document.body;
 
-    if (scrollPosition % 1000 < 400) {
+    if (scrollPosition % 2000 < 500) {
         body.classList.remove('background2');
         body.classList.add('background1');
     } else {
         body.classList.remove('background1');
         body.classList.add('background2');
     }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        burger.classList.toggle('toggle');
+    });
 });
